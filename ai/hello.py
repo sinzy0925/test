f983dc6b31ca7f396d284c6b3ca6f208e94a8d7e
sinzy0925 <84183154+sinzy0925@users.noreply.github.com>
2024-10-17 15:44:29 +0900

    Add files via upload

--- ai/hello.py
+++ ai/hello.py
@@ -1,6 +1,11 @@
-# api/hello.py
-def handler(request):
-    return {
-        'statusCode': 200,
-        'body': 'Hello, Serverless!'
-    }
+# app.py
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+ 
+if __name__ == '__main__':
+    app.run()
